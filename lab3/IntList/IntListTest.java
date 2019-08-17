aba2f1b67763b29e7867c5eb745f564699f7035d
Dawei Gu <gugugudawei@gmail.com>
2019-08-17 13:52:57 +0800

    Finished Lab3 but cannot figure out how to write a destructive method for reversing an IntList (Mine is non-destructive)

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,13 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3, 4, 5, 6);
+        IntList emptyList = IntList.of();
+        assertEquals(IntList.reverse(A), IntList.of(6, 5, 4, 3, 2, 1));
+        //assertNotEquals(A, IntList.of(1, 2, 3, 4, 5, 6));
+        assertNull(IntList.reverse(emptyList));
+    }
+
 }
