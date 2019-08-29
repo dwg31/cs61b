604692ce54cada840baa4531c7ca8c6774eb0b8e
Dawei Gu <gugugudawei@gmail.com>
2019-08-29 10:59:40 +0800

    Completed Proj 2A with style check

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,29 +1,58 @@
 package bearmaps;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Collections;
+
+/**
+ * Using ArrayList to implement heap minimum priority queue.
+ *
+ * @author Dawei Gu
+ */
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
-    ArrayList<Node<T>> heap;
-    Map<T, Integer> indexMap;
+    /** ArrayList enabled heap. */
+    private ArrayList<Node<T>> heap;
+    /** a HashMap that stores item and item position
+     * in the heap entries.
+     */
+    private Map<T, Integer> indexMap;
 
-    static class Node<T> {
+    /** every Node stores the item and its priority. */
+    private static class Node<T> {
+        /** item of any type. */
         private T item;
+        /** priority of this item. */
         private double priority;
 
+        /** default constructor.
+         *
+         * @param i item
+         * @param p priority
+         */
         Node(T i, double p) {
             this.item = i;
             this.priority = p;
         }
 
+        /**
+         * @return the item in this Node.
+         */
         T getItem() {
             return item;
         }
 
+        /**
+         * @return the priority of the item in this Node.
+         */
         double getPriority() {
             return priority;
         }
     }
 
+    /** default constructor. */
     ArrayHeapMinPQ() {
         heap = new ArrayList<>();
         indexMap = new HashMap<>();
@@ -84,12 +113,22 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /** swap the position of two items in the heap.
+     *
+     * @param c the index of the first item.
+     * @param p the index of the second item.
+     */
     private void swap(int c, int p) {
         Collections.swap(heap, c, p);
         indexMap.put(heap.get(c).getItem(), c);
         indexMap.put(heap.get(p).getItem(), p);
     }
 
+    /** swim item up if the priority of this item is
+     * smaller than its parent.
+     *
+     * @param c the index of the current item
+     */
     private void swim(int c) {
         while (c > 0 && smaller(c, parent(c))) {
             int p = parent(c);
@@ -98,6 +137,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /** sink item down if the priority of this item
+     * greater than either of its children.
+     *
+     * @param p the index of the current item
+     */
     private void sink(int p) {
         while (leftChild(p) < size()) {
             int left = leftChild(p);
@@ -114,19 +158,54 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /**
+     * @param c the index of the first item
+     * @param p the index of the second item
+     * @return true if the priority of the first item is smaller
+     *         than that of the second.
+     */
     private boolean smaller(int c, int p) {
         return heap.get(c).getPriority() < heap.get(p).getPriority();
     }
 
+    /**
+     * @param i the index of the given item.
+     * @return the index of the parent of the given item.
+     */
     private int parent(int i) {
         return (i - 1) / 2;
     }
 
+    /**
+     * @param i the index of the given item.
+     * @return the index of the left child of the given item.
+     */
     private int leftChild(int i) {
         return 2 * i + 1;
     }
 
+    /**
+     * @param i the index of the given item.
+     * @return the index of the right child of the given item.
+     */
     private int rightChild(int i) {
         return 2 * i + 2;
+    }
+
+    /**
+     * @return true if every item's priority is less than or
+     * equal to both of its children.
+     *
+     * this method is DESTRUCTIVE.
+     */
+    boolean priorityCheck() {
+        boolean checked = true;
+        while (size() > 1) {
+            double parentPriority = heap.get(0).getPriority();
+            removeSmallest();
+            double childPriority = heap.get(0).getPriority();
+            checked &= parentPriority <= childPriority;
+        }
+        return checked;
     }
 }
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,27 +1,25 @@
 package bearmaps;
 
 import org.junit.Test;
-
-import java.lang.reflect.Array;
 import edu.princeton.cs.algs4.Stopwatch;
-
 import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
-
-    private void speedHelp(ExtrinsicMinPQ<String> PQ, int n) {
+    private void speedHelp(ExtrinsicMinPQ<String> pq, int n) {
         Stopwatch sw = new Stopwatch();
         for (int i = 0; i < n; i += 1) {
-            PQ.add("hi" + i, Math.random());
+            pq.add("hi" + i, Math.random());
         }
-        System.out.println("Adding " + n + " items costs " + PQ.getClass() + ' ' + sw.elapsedTime() + " seconds.");
+        System.out.println("Adding " + n + " items costs "
+                + pq.getClass() + ' ' + sw.elapsedTime() + " seconds.");
         sw = new Stopwatch();
         for (int i = 0; i < n / 2; i += 1) {
-            PQ.removeSmallest();
+            pq.removeSmallest();
         }
-        System.out.println("Removing " + n / 2 + " items costs " + PQ.getClass() + ' ' + sw.elapsedTime() + " seconds.");
+        System.out.println("Removing " + n / 2 + " items costs "
+                + pq.getClass() + ' ' + sw.elapsedTime() + " seconds.");
         for (int i = 0; i < n / 2; i += 1) {
-            PQ.removeSmallest();
+            pq.removeSmallest();
         }
         System.out.println();
     }
@@ -46,40 +44,36 @@ public class ArrayHeapMinPQTest {
 
     @Test
     public void testRemoveSmallest() {
-        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
         for (int i = 0; i < 10000; i += 1) {
-            PQ.add("hi" + i, Math.random() * 100);
+            pq.add("hi" + i, Math.random() * 100);
         }
         for (int i = 0; i < 5000; i += 1) {
-            PQ.removeSmallest();
+            pq.removeSmallest();
         }
-        assertEquals(5000, PQ.size());
+        assertEquals(5000, pq.size());
     }
 
     @Test
     public void testBasics() {
-        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
         for (int i = 0; i < 1000; i += 1) {
-            PQ.add("hi" + i, i * 0.9);
+            pq.add("hi" + i, i * 0.9);
         }
-        assertEquals(1000, PQ.size());
-        assertEquals("hi0", PQ.getSmallest());
+        assertEquals(1000, pq.size());
+        assertEquals("hi0", pq.getSmallest());
     }
 
     @Test
     public void testChangePriority() {
-        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
         for (int i = 0; i < 4000; i += 1) {
-            PQ.add("hi" + i, i);
+            pq.add("hi" + i, i);
         }
         for (int i = 0; i < 3200; i += 1) {
-            PQ.changePriority("hi" + (int) (Math.random() * 1000), Math.random() * 1000);
+            pq.changePriority("hi" + (int) (Math.random() * 1000),
+                    Math.random() * 1000);
         }
-        while(PQ.size() > 1) {
-            double parentPriority = PQ.heap.get(0).getPriority();
-            PQ.removeSmallest();
-            double childPriority = PQ.heap.get(0).getPriority();
-            assertTrue(parentPriority <= childPriority);
-        }
+        assertTrue(pq.priorityCheck());
     }
 }
