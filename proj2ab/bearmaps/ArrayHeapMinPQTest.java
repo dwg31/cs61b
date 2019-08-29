af30cf5189062aaae88a868b229ed8b02aad930d
Dawei Gu <gugugudawei@gmail.com>
2019-08-29 10:25:07 +0800

    Completed ArrayHeapMinPQ, but haven't tested speed yet.

--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -3,10 +3,58 @@ package bearmaps;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
+import edu.princeton.cs.algs4.Stopwatch;
 
 import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
+
+    private void speedHelp(ExtrinsicMinPQ<String> PQ, int n) {
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < n; i += 1) {
+            PQ.add("hi" + i, Math.random());
+        }
+        System.out.println("Adding " + n + " items costs " + PQ.getClass() + ' ' + sw.elapsedTime() + " seconds.");
+        sw = new Stopwatch();
+        for (int i = 0; i < n / 2; i += 1) {
+            PQ.removeSmallest();
+        }
+        System.out.println("Removing " + n / 2 + " items costs " + PQ.getClass() + ' ' + sw.elapsedTime() + " seconds.");
+        for (int i = 0; i < n / 2; i += 1) {
+            PQ.removeSmallest();
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void testSpeed() {
+        ExtrinsicMinPQ<String> heap = new ArrayHeapMinPQ<>();
+        ExtrinsicMinPQ<String> naive = new NaiveMinPQ<>();
+        speedHelp(heap, 10);
+        speedHelp(naive, 10);
+        speedHelp(heap, 1000);
+        speedHelp(naive, 1000);
+        speedHelp(heap, 2000);
+        speedHelp(naive, 2000);
+        speedHelp(heap, 4000);
+        speedHelp(naive, 4000);
+        speedHelp(heap, 8000);
+        speedHelp(naive, 8000);
+        speedHelp(heap, 16000);
+        speedHelp(naive, 16000);
+    }
+
+    @Test
+    public void testRemoveSmallest() {
+        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
+        for (int i = 0; i < 10000; i += 1) {
+            PQ.add("hi" + i, Math.random() * 100);
+        }
+        for (int i = 0; i < 5000; i += 1) {
+            PQ.removeSmallest();
+        }
+        assertEquals(5000, PQ.size());
+    }
 
     @Test
     public void testBasics() {
@@ -16,113 +64,6 @@ public class ArrayHeapMinPQTest {
         }
         assertEquals(1000, PQ.size());
         assertEquals("hi0", PQ.getSmallest());
-    }
-
-    @Test
-    public void testSpeed() {
-        System.out.println("Testing ArrayHeapMinPQ speed...");
-        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 2000; i += 1) {
-            PQ.add("hi" + i, Math.random() * 10);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("Inserting 2000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 2000; i < 6000; i += 1) {
-            PQ.add("hi" + i, Math.random() * 10);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Inserting 4000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 6000; i < 22000; i += 1) {
-            PQ.add("hi" + i, Math.random() * 10);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Inserting 16000 items costs " + (end - start) / 1000.0 + " seconds.");
-        System.out.println("Does inserting look logarithmic?");
-        System.out.println();
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 2000; i += 1) {
-            PQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 2000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        for (int i = 0; i < 4000; i += 1) {
-            PQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 4000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 16000; i += 1) {
-            PQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 16000 items costs " + (end - start) / 1000.0 + " seconds.");
-        System.out.println("Does Removing look logarithmic?");
-        System.out.println();
-
-        System.out.println("Testing NaiveMinPQ speed...");
-        NaiveMinPQ<String> NPQ = new NaiveMinPQ<>();
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 2000; i += 1) {
-            NPQ.add("hi" + i, Math.random() * 10);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Inserting 2000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 2000; i < 6000; i += 1) {
-            NPQ.add("hi" + i, Math.random() * 10);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Inserting 4000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 6000; i < 22000; i += 1) {
-            NPQ.add("hi" + i, Math.random() * 10);
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Inserting 16000 items costs " + (end - start) / 1000.0 + " seconds.");
-        System.out.println();
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 2000; i += 1) {
-            NPQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 2000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        for (int i = 0; i < 4000; i += 1) {
-            NPQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 4000 items costs " + (end - start) / 1000.0 + " seconds.");
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 16000; i += 1) {
-            NPQ.removeSmallest();
-        }
-        end = System.currentTimeMillis();
-        System.out.println("Removing 16000 items costs " + (end - start) / 1000.0 + " seconds.");
-        System.out.println();
-    }/** !!!!!!!!!!!!!!!!!!测试remove的方法不对!!!!!!!!!!!!!!!!!!!!!!!! */
-
-    @Test
-    public void testRemoveSmallest() {
-        ArrayHeapMinPQ<String> PQ = new ArrayHeapMinPQ<>();
-        for (int i = 0; i < 10000; i += 1) {
-            PQ.add("hi" + i, Math.random() * 100);
-        }
-        for (int i = 0; i < 10000; i += 1) {
-            PQ.removeSmallest();
-        }
-        assertEquals(0, PQ.size());
     }
 
     @Test
